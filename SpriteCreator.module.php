da985af12d4de2bffa86efe8a2d69b60fb47f78c
Ivan Milincic <kreativan@outlook.com>
2021-04-14 13:24:46 +0200

    update

--- SpriteCreator.module.php
+++ SpriteCreator.module.php
@@ -28,14 +28,6 @@ class SpriteCreator extends WireData implements Module {
     $this->tmpl_path = $this->config->paths->templates;
     $this->tmpl_url = $this->config->urls->templates;
 
-    $this->srcDirectory = $this->tmpl_path . $this->src_directory;
-    $this->spriteDirectory = !empty($this->sprite_directory) ? $this->tmpl_path . $this->sprite_directory : $this->tmpl_path . "sprite";
-
-    $this->spriteFilepath = !empty($this->sprite_filepath) ? $this->tmpl_url . $this->sprite_filepath : $this->tmpl_url . "sprite";
-    $this->spriteFilename = !empty($this->sprite_filename) ? $this->sprite_filename : "icon-sprite";
-
-    $this->cssDirectory = !empty($this->css_directory) ? $this->tmpl_url . $this->css_directory : $this->tmpl_path . "sprite";
-
   }
 
   public function generate() {
@@ -53,10 +45,10 @@ class SpriteCreator extends WireData implements Module {
       "forceGenerate" => $this->force_generate == "1" ? true : false,
 
       "srcDirectory" => $this->tmpl_path . $this->src_directory,
-      "spriteDirectory" => $this->spriteDirectory,
+      "spriteDirectory" => !empty($this->sprite_directory) ? $this->tmpl_path . $this->sprite_directory : $this->tmpl_path . "sprite",
 
-      "spriteFilepath" => $this->spriteFilepath,
-      "spriteFilename" => $this->spriteFilename, 
+      "spriteFilepath" => !empty($this->sprite_filepath) ? $this->tmpl_url . $this->sprite_filepath : $this->tmpl_url . "sprite",
+      "spriteFilename" => !empty($this->sprite_filename) ? $this->sprite_filename : "icon-sprite",
 
       "tileMargin" => 0,  
       "retina" => array(1, 1), 
@@ -67,7 +59,7 @@ class SpriteCreator extends WireData implements Module {
 
       "targets" => array(
         array(
-          "cssDirectory" => $this->cssDirectory,
+          "cssDirectory" => !empty($this->css_directory) ? $this->tmpl_path . $this->css_directory : $this->tmpl_path . "sprite",
           "cssFilename" => !empty($this->css_filename) ? $this->css_filename : "icon-sprite.css",
         )
       )
